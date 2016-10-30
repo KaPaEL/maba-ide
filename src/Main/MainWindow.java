5f678f1b3d98dfdfdbd6933b53774d120c91a7d7
hanifsudira <sudirahanif@gmail.com>
2016-10-30 20:17:27 +0700

    project requirement

--- src/Main/MainWindow.java
+++ src/Main/MainWindow.java
@@ -1,7 +1,24 @@
 package Main;
 
+import javax.swing.*;
+
 /**
  * Created by Hanif Sudira on 10/30/2016.
  */
-public class MainWindow {
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        InitUI();
+    }
+
+    private void InitUI(){
+
+    }
+
+    public static void main(String args[]){
+        MainWindow mainWindow = new MainWindow();
+        JFrame frame = new JFrame("Maba IDE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(720,640);
+        frame.setVisible(true);
+    }
 }
