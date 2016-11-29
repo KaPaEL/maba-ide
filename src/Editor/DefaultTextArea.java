6d27d94502286f42e25eb92cd0b23fe2309bfd9f
Muhammad Izzuddin <demsyx707@gmail.com>
2016-11-29 19:42:35 +0700

    thread jadi daemon

--- src/Editor/DefaultTextArea.java
+++ src/Editor/DefaultTextArea.java
@@ -23,14 +23,15 @@ public class DefaultTextArea extends RSyntaxTextArea implements ITextArea {
 
     static int counter = 0;
     int flag = 0;
+    int flagThread = 0;
     static Timer timer;
     public DefaultTextArea() {
         this.setSize(20,60);
         this.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
         this.setCodeFoldingEnabled(true);
         this.setAutoscrolls(true);
-        //commandRedoStack = null;
         commandUndoStack.push(GetText());
+
 
         this.addKeyListener(new KeyListener() {
             @Override
@@ -40,6 +41,46 @@ public class DefaultTextArea extends RSyntaxTextArea implements ITextArea {
 
             @Override
             public void keyPressed(KeyEvent keyEvent) {
+                if(flagThread==0)
+                {
+                    System.out.println("flag masuk");
+                    flagThread = 1;
+                    counter=0;
+                    TimerTask timerTask = new TimerTask() {
+                        @Override
+                        public void run() {
+                            counter++;
+                        }
+                    };
+                    Thread t = new Thread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            while (true) {
+                                try {
+                                    if(counter==2)
+                                    {
+                                        System.out.println("masuk thread");
+                                        if(!commandUndoStack.peek().equals(GetText()) && commandRedoStack.isEmpty())
+                                        {
+                                            commandUndoStack.push(GetText());
+                                        }
+                                        counter = 0;
+                                    }
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        }
+                    });
+                    t.interrupt();
+                    timer = new Timer("MyTimer");
+                    timer.scheduleAtFixedRate(timerTask, 0, 1000);
+                    t.setDaemon(true);
+                    t.start();
+                    System.out.println(t.getState());
+                }
                 if(commandUndoStack.size()==1 && flag == 0)
                 {
                     commandUndoStack.push(GetText());
@@ -63,6 +104,18 @@ public class DefaultTextArea extends RSyntaxTextArea implements ITextArea {
                     }
                 }
 
+                else if (keyEvent.getKeyCode() == KeyEvent.VK_SPACE) {
+                    if(!commandUndoStack.peek().equals(GetText().trim())  && commandRedoStack.isEmpty() )
+                    {
+                        commandUndoStack.push(GetText());
+                    }
+                }
+                else if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
+                    if(!commandUndoStack.peek().equals(GetText().trim().replace("\n", "").replace("\r", ""))  && commandRedoStack.isEmpty() )
+                    {
+                        commandUndoStack.push(GetText());
+                    }
+                }
             }
 
             @Override
@@ -74,38 +127,7 @@ public class DefaultTextArea extends RSyntaxTextArea implements ITextArea {
                         commandRedoStack.pop();
                     }
                 }
-                counter=0;
-                TimerTask timerTask = new TimerTask() {
-                    @Override
-                    public void run() {
-                        counter++;
-                    }
-                };
-                Thread t = new Thread(new Runnable() {
 
-                    @Override
-                    public void run() {
-                        while (true) {
-                            try {
-                                if(counter==4)
-                                {
-                                    if(!commandUndoStack.peek().equals(GetText()) && commandRedoStack.isEmpty())
-                                    {
-                                        commandUndoStack.push(GetText());
-                                        timer.cancel();
-                                        break;
-                                    }
-                                }
-                                Thread.sleep(1000);
-                            } catch (InterruptedException ex) {
-                                ex.printStackTrace();
-                            }
-                        }
-                    }
-                });
-                timer = new Timer("MyTimer");
-                timer.scheduleAtFixedRate(timerTask, 0, 1000);
-                t.start();
             }
         });
     }
